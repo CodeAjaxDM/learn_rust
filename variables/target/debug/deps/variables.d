967e937a340e7e2120a89941979e1a2fa76a7612
Adam Wagener <Adam_wagener@selinc.com>
2023-07-11 16:53:47 -0700

    simple mutability sample system added

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\adamwage\repos\learn_rust\learn_rust\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\adamwage\repos\learn_rust\learn_rust\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\adamwage\repos\learn_rust\learn_rust\variables\target\debug\variables.exe: C:\Users\adamwage\repos\learn_rust\learn_rust\variables\src\main.rs
