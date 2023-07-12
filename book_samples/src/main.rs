bebcb27b7e41f11ee47cb0221e21128daec44e95
Adam Wagener <Adam_wagener@selinc.com>
2023-07-12 12:07:11 -0700

    expressions, statments, and retuns example

--- book_samples/src/main.rs
+++ book_samples/src/main.rs
@@ -2,8 +2,14 @@ fn main() {
     println!("Hello, world!");
 
     another_function(5, 'm');
+    let x:i32 = five();
+    println!("the value of x is: {x}");
 }
 
 fn another_function(value: i32, unit_lable: char) {
     println!("The measurment is: {value}{unit_lable}");
+}
+
+fn five() -> i32 {
+    5
 }
