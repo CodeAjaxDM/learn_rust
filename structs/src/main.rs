220cf0fffaa8d9872076723dbe14c438604805d1
Adam Wagener <Adam_wagener@selinc.com>
2023-07-13 16:10:12 -0700

    recatangle sample struct implimented

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,6 +5,30 @@ struct Rectangle
     width: u32,
 }
 
+impl Rectangle
+{
+    fn area(&self) -> u32
+    {
+        self.height * self.width
+    }
+
+    fn has_width(&self) -> bool
+    {
+        self.width > 0
+    }
+
+    fn has_height(&self) -> bool
+    {
+        self.height > 0
+    }
+
+    fn can_hold(&self, rectangle: &Rectangle) -> bool
+    {
+        rectangle.width <= self.width && rectangle.height <= self.height
+    }
+}
+
+
 fn main() 
 {
     let rect_1 = Rectangle
@@ -13,11 +37,23 @@ fn main()
         width: 10,
     };
 
+    let rect_2 = Rectangle
+    {
+        height: 10,
+        width: 10,
+    };
+
+    let rect_3 = Rectangle
+    {
+        height: 10,
+        width: 12,
+    };
+
     println!("The area of the recangle is {} square pixels.",
-        area(&rect_1 ));
+        rect_1.area());
+
+        println!("Can rect2 fit in rect1? {}", rect_1.can_hold(&rect_2));
+        println!("Can rect3 fit in rect1? {}", rect_1.can_hold(&rect_3));
 }
 
-fn area(rectangle: &Rectangle) -> u32
-{
-    rectangle.height * rectangle.width
-}
+
